1d4d32074f2575f137c6e00fe53adaac53f46266
Nathan Townsend <ntownsend2@mt.gov>
2014-04-15 08:53:18 -0600

    Included AjaxResult

---
+++ AjaxResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpencutInternal.Models
+{
+    public class AjaxResult
+    {
+        AjaxStatus _status;
+
+        public AjaxResult(AjaxStatus status, string message)
+        {
+            Message = message;
+            _status = status;
+        }
+
+        public string Message { get; set; }
+
+        public string Status { get { return _status.ToString(); } }
+
+        // remember to keep in sync the javascript version of this enum 
+        public enum AjaxStatus
+        {
+            OK,
+            ERROR,
+            UNEXPECTED
+        }
+    }
+}
